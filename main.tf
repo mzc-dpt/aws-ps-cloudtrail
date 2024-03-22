bb46206d1a3a1c83bce104cc9f57cb1a615a137f
Yeji Kim <125236337+yejimz@users.noreply.github.com>
2024-03-22 14:31:53 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,9 +2,9 @@ resource "random_id" "example" {
   byte_length = 8
 }
 
-# resource "aws_kms_key" "kms" {
-#   deletion_window_in_days = 10
-# }
+resource "aws_kms_key" "kms" {
+  deletion_window_in_days = 10
+}
 
 resource "aws_cloudtrail" "example" {
   depends_on = [aws_s3_bucket_policy.example]
@@ -14,7 +14,7 @@ resource "aws_cloudtrail" "example" {
   s3_key_prefix                 = "prefix"
   include_global_service_events = false
   enable_logging = true
-  # kms_key_id = aws_kms_key.kms.key_id
+  kms_key_id = aws_kms_key.kms.key_id
 }
 
 resource "aws_s3_bucket" "example" {
