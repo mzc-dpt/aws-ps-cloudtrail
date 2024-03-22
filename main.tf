24012158e2646735766f3c3fecc4808800f8281b
Yeji Kim <125236337+yejimz@users.noreply.github.com>
2024-03-22 14:42:09 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,6 +4,28 @@ resource "random_id" "example" {
 
 resource "aws_kms_key" "cloudtrail_key" {
   deletion_window_in_days = 10
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Id      = "key-policy",
+    Statement = [
+      {
+        Sid       = "Allow CloudTrail to use the key",
+        Effect    = "Allow",
+        Principal = {
+          Service = "cloudtrail.amazonaws.com"
+        },
+        Action    = [
+          "kms:Encrypt",
+          "kms:Decrypt",
+          "kms:ReEncrypt*",
+          "kms:GenerateDataKey*",
+          "kms:DescribeKey"
+        ],
+        Resource  = "*"
+      }
+    ]
+  })
 }
 
 resource "aws_cloudtrail" "example" {
@@ -23,53 +45,28 @@ resource "aws_s3_bucket" "example" {
   force_destroy = true
 }
 
-data "aws_iam_policy_document" "example" {
-  statement {
-    sid    = "AWSCloudTrailAclCheck"
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["cloudtrail.amazonaws.com"]
-    }
-
-    actions   = ["s3:GetBucketAcl"]
-    resources = [aws_s3_bucket.example.arn]
-    condition {
-      test     = "StringEquals"
-      variable = "aws:SourceArn"
-      values   = ["arn:${data.aws_partition.current.partition}:cloudtrail:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:trail/example"]
-    }
-  }
-
-  statement {
-    sid    = "AWSCloudTrailWrite"
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["cloudtrail.amazonaws.com"]
-    }
-
-    actions   = ["s3:PutObject"]
-    resources = ["${aws_s3_bucket.example.arn}/prefix/AWSLogs/${data.aws_caller_identity.current.account_id}/*"]
-
-    condition {
-      test     = "StringEquals"
-      variable = "s3:x-amz-acl"
-      values   = ["bucket-owner-full-control"]
-    }
-    condition {
-      test     = "StringEquals"
-      variable = "aws:SourceArn"
-      values   = ["arn:${data.aws_partition.current.partition}:cloudtrail:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:trail/example"]
-    }
-  }
-}
-
 resource "aws_s3_bucket_policy" "example" {
   bucket = aws_s3_bucket.example.id
-  policy = data.aws_iam_policy_document.example.json
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid       = "AWSCloudTrailWrite",
+        Effect    = "Allow",
+        Principal = {
+          Service = "cloudtrail.amazonaws.com"
+        },
+        Action    = "s3:PutObject",
+        Resource  = "${aws_s3_bucket.example.arn}/prefix/AWSLogs/${data.aws_caller_identity.current.account_id}/*",
+        Condition = {
+          StringEquals = {
+            "s3:x-amz-acl"   = "bucket-owner-full-control"
+            "aws:SourceArn" = "arn:${data.aws_partition.current.partition}:cloudtrail:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:trail/example"
+          }
+        }
+      }
+    ]
+  })
 }
 
 data "aws_caller_identity" "current" {}
