d365d3a1c6671b1dd71309c2d0c10766230bc576
Yeji Kim <125236337+yejimz@users.noreply.github.com>
2024-03-22 13:56:29 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,32 +2,71 @@ resource "random_id" "example" {
   byte_length = 8
 }
 
-resource "aws_s3_bucket" "example" {
-  bucket = "example-bucket-${random_id.example.hex}"
+resource "aws_cloudtrail" "example" {
+  depends_on = [aws_s3_bucket_policy.example]
+
+  name                          = "example"
+  s3_bucket_name                = aws_s3_bucket.example.id
+  s3_key_prefix                 = "prefix"
+  include_global_service_events = false
 }
 
-resource "aws_s3_bucket_ownership_controls" "example" {
-  bucket = aws_s3_bucket.example.id
-  rule {
-    object_ownership = "BucketOwnerPreferred"
+resource "aws_s3_bucket" "example" {
+  bucket        = "example-bucket-${random_id.example.hex}"
+  force_destroy = true
+}
+
+data "aws_iam_policy_document" "example" {
+  statement {
+    sid    = "AWSCloudTrailAclCheck"
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudtrail.amazonaws.com"]
+    }
+
+    actions   = ["s3:GetBucketAcl"]
+    resources = [aws_s3_bucket.example.arn]
+    condition {
+      test     = "StringEquals"
+      variable = "aws:SourceArn"
+      values   = ["arn:${data.aws_partition.current.partition}:cloudtrail:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:trail/example"]
+    }
+  }
+
+  statement {
+    sid    = "AWSCloudTrailWrite"
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudtrail.amazonaws.com"]
+    }
+
+    actions   = ["s3:PutObject"]
+    resources = ["${aws_s3_bucket.example.arn}/prefix/AWSLogs/${data.aws_caller_identity.current.account_id}/*"]
+
+    condition {
+      test     = "StringEquals"
+      variable = "s3:x-amz-acl"
+      values   = ["bucket-owner-full-control"]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "aws:SourceArn"
+      values   = ["arn:${data.aws_partition.current.partition}:cloudtrail:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:trail/example"]
+    }
   }
 }
 
-resource "aws_s3_bucket_acl" "example" {
-  depends_on = [aws_s3_bucket_ownership_controls.example]
-
+resource "aws_s3_bucket_policy" "example" {
   bucket = aws_s3_bucket.example.id
-  acl    = "private"
+  policy = data.aws_iam_policy_document.example.json
 }
 
-resource "aws_cloudwatch_log_group" "example" {
-  name = "Example"
-}
+data "aws_caller_identity" "current" {}
 
-# Create an AWS CloudTrail trail
-resource "aws_cloudtrail" "example_cloudtrail" {
-  name                          = "example-cloudtrail"
-  s3_bucket_name                = aws_s3_bucket.example.id
-  include_global_service_events = true
-  # enable_logging = var.enable_logging
-}
+data "aws_partition" "current" {}
+
+data "aws_region" "current" {}
