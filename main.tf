74413c513f94c5611c6c87d6688997ca75c823b7
Yeji Kim <125236337+yejimz@users.noreply.github.com>
2024-03-25 17:27:57 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,6 +11,10 @@ resource "aws_cloudtrail" "example" {
   include_global_service_events = true
   is_multi_region_trail         = true
   enable_log_file_validation = true
+
+  depends_on = [
+    aws_s3_bucket.example
+  ]
 }
 
 resource "aws_s3_bucket" "example" {
