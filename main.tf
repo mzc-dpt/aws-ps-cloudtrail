adbd4669acecdcab54ff30a8334ec585b752859b
Yeji Kim <125236337+yejimz@users.noreply.github.com>
2024-03-26 15:38:20 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,19 @@
-resource "random_id" "example" {
+resource "random_id" "bucket-suffix" {
   byte_length = 8
 }
 
-resource "aws_s3_bucket" "example" {
-  bucket        = "example-bucket-${random_id.example.hex}"
+resource "aws_s3_bucket" "cloudtrail-logs" {
+  bucket        = "cloudtrail-logs-${random_id.bucket-suffix.hex}"
   # bucket        = "example-bucket-test123455"
   force_destroy = true
 }
 
 resource "aws_cloudtrail" "example" {
-  # depends_on = [aws_s3_bucket_policy.example, aws_s3_bucket.example]
+  # depends_on = [aws_s3_bucket_policy.example, aws_s3_bucket.cloudtrail-logs]
   depends_on = [aws_s3_bucket_policy.example]
 
   name                          = "example"
-  s3_bucket_name                = aws_s3_bucket.example.id
+  s3_bucket_name                = aws_s3_bucket.cloudtrail-logs.id
   s3_key_prefix                 = "prefix"
   include_global_service_events = false
   enable_log_file_validation = true
@@ -30,7 +30,7 @@ data "aws_iam_policy_document" "example" {
     }
 
     actions   = ["s3:GetBucketAcl"]
-    resources = [aws_s3_bucket.example.arn]
+    resources = [aws_s3_bucket.cloudtrail-logs.arn]
     condition {
       test     = "StringEquals"
       variable = "aws:SourceArn"
@@ -48,7 +48,7 @@ data "aws_iam_policy_document" "example" {
     }
 
     actions   = ["s3:PutObject"]
-    resources = ["${aws_s3_bucket.example.arn}/prefix/AWSLogs/${data.aws_caller_identity.current.account_id}/*"]
+    resources = ["${aws_s3_bucket.cloudtrail-logs.arn}/prefix/AWSLogs/${data.aws_caller_identity.current.account_id}/*"]
     condition {
       test     = "StringEquals"
       variable = "s3:x-amz-acl"
@@ -63,7 +63,7 @@ data "aws_iam_policy_document" "example" {
 }
 
 resource "aws_s3_bucket_policy" "example" {
-  bucket = aws_s3_bucket.example.id
+  bucket = aws_s3_bucket.cloudtrail-logs.id
   policy = data.aws_iam_policy_document.example.json
 }
 
