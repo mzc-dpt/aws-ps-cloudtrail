3c919e35e3194da8a68c65b6736df4a3bff15d69
Yeji Kim <125236337+yejimz@users.noreply.github.com>
2024-03-26 12:46:27 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,58 +19,55 @@ resource "aws_s3_bucket" "example" {
 }
 
 data "aws_iam_policy_document" "example" {
-  statement {
-    sid    = "AWSCloudTrailAclCheck"
-    effect = "Allow"
+//   statement {
+//     sid    = "AWSCloudTrailAclCheck"
+//     effect = "Allow"
 
-    principals {
-      type        = "Service"
-      identifiers = ["cloudtrail.amazonaws.com"]
-    }
+//     principals {
+//       type        = "Service"
+//       identifiers = ["cloudtrail.amazonaws.com"]
+//     }
 
-    actions   = ["s3:GetBucketAcl"]
-    resources = [aws_s3_bucket.example.arn]
-    condition {
-      test     = "StringEquals"
-      variable = "aws:SourceArn"
-      # values   = ["arn:${data.aws_partition.current.partition}:cloudtrail:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:trail/example"]
-      values   = ["arn:aws:cloudtrail:ap-northeast-2:544093516502:trail/example"]
-    }
-  }
+//     actions   = ["s3:GetBucketAcl"]
+//     resources = [aws_s3_bucket.example.arn]
+//     condition {
+//       test     = "StringEquals"
+//       variable = "aws:SourceArn"
+//       values   = ["arn:${data.aws_partition.current.partition}:cloudtrail:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:trail/example"]
+//     }
+//   }
 
-  statement {
-    sid    = "AWSCloudTrailWrite"
-    effect = "Allow"
+//   statement {
+//     sid    = "AWSCloudTrailWrite"
+//     effect = "Allow"
 
-    principals {
-      type        = "Service"
-      identifiers = ["cloudtrail.amazonaws.com"]
-    }
+//     principals {
+//       type        = "Service"
+//       identifiers = ["cloudtrail.amazonaws.com"]
+//     }
 
-    actions   = ["s3:PutObject"]
-    # resources = ["${aws_s3_bucket.example.arn}/prefix/AWSLogs/${data.aws_caller_identity.current.account_id}/*"]
-    resources = ["${aws_s3_bucket.example.arn}/prefix/AWSLogs/544093516502/*"]
-    condition {
-      test     = "StringEquals"
-      variable = "s3:x-amz-acl"
-      values   = ["bucket-owner-full-control"]
-    }
-    condition {
-      test     = "StringEquals"
-      variable = "aws:SourceArn"
-      # values   = ["arn:${data.aws_partition.current.partition}:cloudtrail:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:trail/example"]
-      values   = ["arn:aws:cloudtrail:ap-northeast-2:544093516502:trail/example"]
-    }
-  }
-}
+//     actions   = ["s3:PutObject"]
+//     resources = ["${aws_s3_bucket.example.arn}/prefix/AWSLogs/${data.aws_caller_identity.current.account_id}/*"]
+//     condition {
+//       test     = "StringEquals"
+//       variable = "s3:x-amz-acl"
+//       values   = ["bucket-owner-full-control"]
+//     }
+//     condition {
+//       test     = "StringEquals"
+//       variable = "aws:SourceArn"
+//       values   = ["arn:${data.aws_partition.current.partition}:cloudtrail:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:trail/example"]
+//     }
+//   }
+// }
 
-resource "aws_s3_bucket_policy" "example" {
-  bucket = aws_s3_bucket.example.id
-  policy = data.aws_iam_policy_document.example.json
-}
+// resource "aws_s3_bucket_policy" "example" {
+//   bucket = aws_s3_bucket.example.id
+//   policy = data.aws_iam_policy_document.example.json
+// }
 
-data "aws_caller_identity" "current" {}
+// data "aws_caller_identity" "current" {}
 
-data "aws_partition" "current" {}
+// data "aws_partition" "current" {}
 
-data "aws_region" "current" {}
+// data "aws_region" "current" {}
