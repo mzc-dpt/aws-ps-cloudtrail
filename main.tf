754f773c6e54840504dd07c435d174ddb317e621
Yeji Kim <125236337+yejimz@users.noreply.github.com>
2024-03-26 13:21:36 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,27 +1,74 @@
-resource "aws_security_group" "example" {
-  name   = "sentinel-test-sg"
-  description = "Example security group for EC2 instances"
+resource "random_id" "example" {
+  byte_length = 8
+}
 
-  ingress {
-    from_port   = 8081
-    to_port     = 8081
-    protocol    = "tcp"
-    
-    ## 보안 취약점: 인터넷에 대한 액세스를 제어하지 않음
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "Allow all inbound traffic"
+resource "aws_s3_bucket" "example" {
+  bucket        = "example-bucket-${random_id.example.hex}"
+  # bucket        = "example-bucket-test123455"
+  force_destroy = true
+}
+
+resource "aws_cloudtrail" "example" {
+  # depends_on = [aws_s3_bucket_policy.example, aws_s3_bucket.example]
+  depends_on = [aws_s3_bucket_policy.example]
+
+  name                          = "example"
+  s3_bucket_name                = aws_s3_bucket.example.id
+  s3_key_prefix                 = "prefix"
+  include_global_service_events = false
+  enable_log_file_validation = true
+}
+
+data "aws_iam_policy_document" "example" {
+  statement {
+    sid    = "AWSCloudTrailAclCheck"
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudtrail.amazonaws.com"]
+    }
+
+    actions   = ["s3:GetBucketAcl"]
+    resources = [aws_s3_bucket.example.arn]
+    condition {
+      test     = "StringEquals"
+      variable = "aws:SourceArn"
+      values   = ["arn:${data.aws_partition.current.partition}:cloudtrail:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:trail/example"]
+    }
   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "Allow all outbound traffic"
-  }
+  statement {
+    sid    = "AWSCloudTrailWrite"
+    effect = "Allow"
 
-  tags = {
-    Name = "sentinel-test-sg"
+    principals {
+      type        = "Service"
+      identifiers = ["cloudtrail.amazonaws.com"]
+    }
+
+    actions   = ["s3:PutObject"]
+    resources = ["${aws_s3_bucket.example.arn}/prefix/AWSLogs/${data.aws_caller_identity.current.account_id}/*"]
+    condition {
+      test     = "StringEquals"
+      variable = "s3:x-amz-acl"
+      values   = ["bucket-owner-full-control"]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "aws:SourceArn"
+      values   = ["arn:${data.aws_partition.current.partition}:cloudtrail:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:trail/example"]
+    }
   }
 }
+
+resource "aws_s3_bucket_policy" "example" {
+  bucket = aws_s3_bucket.example.id
+  policy = data.aws_iam_policy_document.example.json
+}
+
+data "aws_caller_identity" "current" {}
+
+data "aws_partition" "current" {}
+
+data "aws_region" "current" {}
